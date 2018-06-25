782ce298055060a44076fb1d1db232a435e40c5c
Reece Pritchard <rapritchard96@gmail.com>
2018-06-25 12:40:17 +0100

    Updated code in hamming exercise

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,12 +1,11 @@
 def distance(strand_a, strand_b):
-    if len(strand_a) == len(strand_b):
-        if len(strand_a) == 0 and len(strand_b) == 0:
-            return 0
-        else:
-            count = 0
-            for a, b in zip(strand_a, strand_b):
-                if a != b:
-                    count += 1
-        return count
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands must be the same length")
     else:
-        raise ValueError("Stands must be the same length")
+        count = 0
+        for a, b in zip(strand_a, strand_b):
+            if a != b:
+                count += 1
+    return count
+
+       
